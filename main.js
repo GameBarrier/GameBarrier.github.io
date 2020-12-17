c543b30f12919cb2be2829252ee3413029146458
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2020-12-17 00:03:40 -0700

    Bug Fixes, Dottype Button Added, Changelog update

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var visible = false;
+/*var visible = false;
 
 function menu() {
   if (visible == false) {
@@ -13,4 +13,41 @@ function menu() {
     document.querySelector(".menuIcon").style.transform = "rotateZ(0deg)";
     document.querySelector(".menuBar").style.visibility = "hidden";
   }
+}
+*/
+$(function () {
+  materialRipple();
+});
+
+function materialRipple() {
+  $(".fab:not(.disabled)").on("mousedown", function (e) {
+      recycler();
+      var offset = $(this).offset();
+      var XCoord = (e.pageX - offset.left);
+      var YCoord = (e.pageY - offset.top);
+      
+      if ($(this).outerWidth() < 199) {
+          $(this).children().append($("<div class='ripple ripple-active'></div>").css({
+              left: XCoord - 4.5,
+              top: YCoord - 2,
+              height: $(this).width() * .20,
+              width: $(this).width() * .20
+          })).on("mouseup", function () {
+              $(this).children('div').removeClass('ripple-active');
+          });
+      } else {
+          $(this).children().append($("<div class='ripple ripple-active'></div>").css({
+              left: XCoord - 4.5,
+              top: YCoord - 2,
+              height: $(this).width() * .05,
+              width: $(this).width() * .05
+          })).on("mouseup", function () {
+              $(this).children('div').removeClass('ripple-active');
+          });
+      }
+  });
+}
+
+function recycler() {
+  $('html').find('.ripple:not(.active)').remove();
 }
