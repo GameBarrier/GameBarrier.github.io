e13a4f6210e1d74c17d508b1866e21cc2dd14d45
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2020-12-20 12:50:17 -0700

    Update main.js

--- main.js
+++ main.js
@@ -51,3 +51,10 @@ function materialRipple() {
 function recycler() {
   $('html').find('.ripple:not(.active)').remove();
 }
+function SetCookie(name,value,expires,path,domain,secure) {
+  document.cookie = name + "=" + escape(value) +
+  ((expires) ? "; expires=" + expires.toGMTString() : "") +
+  ((path) ? "; path=" + path : "") +
+  ((domain) ? "; domain=" + domain : "") +
+  ((secure) ? "; secure" : "");
+  }
