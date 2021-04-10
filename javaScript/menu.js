254ec40da56ef10d2af01d15287252abb2d8baf2
Kilian Harbauer <>
2021-04-10 18:33:06 +0200

    web updates

--- javaScript/menu.js
+++ javaScript/menu.js
@@ -1,4 +1,4 @@
-var visible = false;
+let visible = false;
 
 function menu() {
   if (visible == false) {
@@ -13,4 +13,31 @@ function menu() {
     document.querySelector(".menuIcon").style.transform = "rotateZ(0deg)";
     document.querySelector(".menuBar").style.visibility = "hidden";
   }
+}
+
+let scrollY = 0;
+
+document.addEventListener('scroll', function(e) {
+
+  if (scrollY < window.scrollY) {
+    closeHeadBar();
+  }
+
+  scrollY = window.scrollY;
+
+  if (scrollY < 200) {
+    openHeadBar();
+  }
+});
+
+function closeHeadBar() {
+  if (scrollY != 0) {
+    document.querySelector(".HeadBar").style.transform = "translateY(-70px)";
+    document.querySelector(".HeadBar").style.transition = "1s";
+  }
+}
+
+function openHeadBar() {
+  document.querySelector(".HeadBar").style.transform = "translateY(0px)";
+  document.querySelector(".HeadBar").style.transition = "0.4s";
 }
--- javaScript/soc_scroll.js
+++ javaScript/soc_scroll.js
@@ -16,81 +16,6 @@ document.querySelector(".SocialsTW").addEventListener("transitionend", function(
   document.querySelector(".SocialsTT").style.transition = "0s";
 });
 
-window.addEventListener('resize', function(event) {
-  if (transTT < 101) {
-    window.scrollTo(0,document.body.scrollHeight);
-  }
-});
-
-window.addEventListener('wheel', function(event) {
-  if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-    if (event.deltaY < 0) {
-
-      if (transTW < 101) {
-        if (twEnd == true) {
-          transTW = transTW + 101;
-          ttEnd = false;
-          twEnd = false;
-          console.log(twEnd);
-          document.getElementById("socialScrollArrowLeft").style.filter = "opacity(70%)";
-          document.getElementById("socialScrollArrowRight").style.filter = "opacity(70%)";
-          document.querySelector(".SocialsTW").style.transition = "0.5s";
-          document.querySelector(".SocialsTW").style.transform = "translate(" + transTW +"vw, -200vh)";
-        }
-      } else if (transTT < 101) {
-        if (ttEnd == true) {
-          transTT = transTT + 101;
-          ttEnd = false;
-          twEnd = false;
-          console.log(ttEnd);
-          document.getElementById("socialScrollArrowLeft").style.filter = "opacity(30%)";
-          document.getElementById("socialScrollArrowRight").style.filter = "opacity(70%)";
-          document.querySelector(".SocialsTT").style.transition = "0.5s";
-          document.querySelector(".SocialsTT").style.transform = "translate(" + transTT +"vw, -100vh)";
-        }
-      } else {
-        if (ttEnd == true) {
-          document.querySelector(".body").style.overflowY = "scroll";
-        }
-      }
-    }
-
-    else if (event.deltaY > 0) {
-
-      if (transTT > 0) {
-        if (ttEnd == true) {
-          transTT = transTT - 101;
-          ttEnd = false;
-          twEnd = false;
-          console.log(ttEnd);
-          document.getElementById("socialScrollArrowLeft").style.filter = "opacity(70%)";
-          document.getElementById("socialScrollArrowRight").style.filter = "opacity(70%)";
-          document.querySelector(".SocialsTT").style.transition = "0.5s";
-          document.querySelector(".body").style.overflowY = "hidden";
-          document.querySelector(".SocialsTT").style.transform = "translate(" + transTT +"vw, -100vh)";
-        }
-      }
-
-      else {
-
-        if (transTW > 0) {
-          if (twEnd == true) {
-            transTW = transTW - 101;
-            ttEnd = false;
-            twEnd = false;
-            console.log(twEnd);
-            document.getElementById("socialScrollArrowLeft").style.filter = "opacity(70%)";
-            document.getElementById("socialScrollArrowRight").style.filter = "opacity(30%)";
-            document.querySelector(".SocialsTW").style.transition = "0.5s";
-            document.querySelector(".body").style.overflowY = "hidden";
-            document.querySelector(".SocialsTW").style.transform = "translate(" + transTW +"vw, -200vh)";
-          }
-        }
-      }
-    }
-  }
-});
-
 function clickScrollRight() {
   if (ttEnd == true && transTT == 101 && transTW == 101 && twEnd == true) {
     transTT = 0;
@@ -98,7 +23,6 @@ function clickScrollRight() {
     document.getElementById("socialScrollArrowLeft").style.filter = "opacity(70%)";
     document.getElementById("socialScrollArrowRight").style.filter = "opacity(70%)";
     document.querySelector(".SocialsTT").style.transition = "0.5s";
-    document.querySelector(".body").style.overflowY = "hidden";
     document.querySelector(".SocialsTT").style.transform = "translate(" + transTT +"vw, -100vh)";
   } else if (ttEnd == true && transTW == 101 && transTT == 0 && twEnd == true) {
     transTW = 0;
@@ -106,7 +30,6 @@ function clickScrollRight() {
     document.getElementById("socialScrollArrowLeft").style.filter = "opacity(70%)";
     document.getElementById("socialScrollArrowRight").style.filter = "opacity(30%)";
     document.querySelector(".SocialsTW").style.transition = "0.5s";
-    document.querySelector(".body").style.overflowY = "hidden";
     document.querySelector(".SocialsTW").style.transform = "translate(" + transTW +"vw, -200vh)";
   }
 }
@@ -118,7 +41,6 @@ function clickScrollLeft() {
     document.getElementById("socialScrollArrowLeft").style.filter = "opacity(30%)";
     document.getElementById("socialScrollArrowRight").style.filter = "opacity(70%)";
     document.querySelector(".SocialsTT").style.transition = "0.5s";
-    document.querySelector(".body").style.overflowY = "hidden";
     document.querySelector(".SocialsTT").style.transform = "translate(" + transTT +"vw, -100vh)";
   } else if (ttEnd == true && transTW == 0 && transTT == 0 && twEnd == true) {
     transTW = 101;
@@ -126,7 +48,6 @@ function clickScrollLeft() {
     document.getElementById("socialScrollArrowLeft").style.filter = "opacity(70%)";
     document.getElementById("socialScrollArrowRight").style.filter = "opacity(70%)";
     document.querySelector(".SocialsTW").style.transition = "0.5s";
-    document.querySelector(".body").style.overflowY = "hidden";
     document.querySelector(".SocialsTW").style.transform = "translate(" + transTW +"vw, -200vh)";
   }
 }
