15157aee84810d9d569b22c72d767dc4e0a88d65
Kilian Harbauer <>
2021-04-15 12:55:30 +0200

    add new features

--- javaScript/slsh.js
+++ javaScript/slsh.js
@@ -1,14 +1,16 @@
 var img01 = document.getElementById("fa01");
 var img02 = document.getElementById("fa02");
 var img03 = document.getElementById("fa03");
+var img04 = document.getElementById("fa04");
 var desc01 = "Art by Baronu3 (www.instagram.com/baronu3/)";
 var desc02 = "Art by Meii";
 var desc03 = "Art by Pixy";
+var desc04 = "Merch design by Meii <3";
 
-var imgs = [img01, img02, img03];
-var imgDesc = [desc01, desc02, desc03];
-var imgIndex = 0;
-var imgLenght = 2;
+var imgs = [img01, img02, img03, img04];
+var imgDesc = [desc01, desc02, desc03, desc04];
+var imgIndex = 2;
+var imgLenght = 3;
 
 function countIndex(direc) {
   if (direc == 0) {
@@ -51,5 +53,12 @@ function imgR() {
       imgs[i].style.position = "static";
       document.getElementById("faCredit").innerHTML = imgDesc[i];
     }
+  }
+}
+
+function enableNavArrow() {
+  var elements = document.getElementsByClassName("faArrowImg");
+  for(var i=0; i<elements.length; i++) {
+      elements[i].style.visibility = "visible";
   }
 }
